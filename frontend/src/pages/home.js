29b0a3d9b4f884977875ff485ebd541f5b3fa585
niralishahh <147350512+niralishahh@users.noreply.github.com>
2023-11-14 20:51:14 -0800

    Add files via upload

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { UserService } from "../service/userService";
 import { EventEmitter } from "../service/eventEmitter";
 import { DataParse } from "../service/dataParse";
+import styles from "./homepage_style.css"
 
 
 function Home() {
@@ -23,7 +24,7 @@ function Home() {
 
 
   return (
-    <div>
+    /* <div>
       <p>
         HOME
       </p>
@@ -36,7 +37,48 @@ function Home() {
       <Link to="/assessments/take-assessment">take assessment page</Link>
       { userData != null && <p>Hi, { userData.username }</p>}
       { userData != null && dataParse.assessmentExists() && <p>Date of most recent assessment: { dataParse.getMostRecentAssessments().date }</p>}
-    </div>
+    </div> */
+    <div>
+      <nav class = "navbar">
+          <ul class = "navbar__menu">
+              <li><a href = "https://www.beatthestreetsca.org/">Back to Website</a></li>
+              <li><a href = "#">Dashboard</a></li>
+              <li><a href = "#">Quick Links</a></li>
+          </ul>
+          <button type = {button} className = {styles.navbar__button} style = "color:white"> Profile</button>
+          
+      </nav>
+
+      <div class = "gradient">
+          <div class = "main__content">
+              <br></br>
+              <h2 style = 'color:white'> Welcome Back!</h2>
+              <div className = {styles.main__tab}>
+                  <div class="rectangle__completed">
+                      <div><h3>Completed</h3></div>
+                  </div>
+                  <div class="rectangle__assigned">
+                      <div><h3>Assignments</h3></div>
+                  </div>
+              </div>
+              <section class="home-page-copy-2-child"></section>
+              <img class="vector-icon" alt="" src="./public/vector.svg" />
+          </div>
+          <div class="frame-parent">
+              <div class="frame">
+                <img class="image-4-icon" alt="" src="./public/image-4@2x.png" />
+              </div>
+              <div class="frame1">
+                <div class="frame2">
+                  <u class="beat-the-streets">Beat the Streets, Inc.</u>
+                  <u class="young-adult-employment"
+                    >Young Adult Employment, Vocational & Life Services</u>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+
   );
 }
 
