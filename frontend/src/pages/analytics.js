05f73a2378c041b4e04453554e7c20c3937f6e09
Boris <boristomov@berkeley.edu>
2023-11-14 19:57:17 -0800

    changes

--- frontend/src/pages/analytics.js
+++ frontend/src/pages/analytics.js
@@ -104,40 +104,46 @@ function Analytics() {
           </div>
         )}
 
+        {/* Conditionally rendered Expand/Exit button */}
+        <button onClick={toggleExpand} className={styles.toggleButton}>
+            {isExpanded ?
+              <img src="http://localhost:3000/backExpandButton.png" alt="Icon Description" className = {styles.toggleOff}></img> 
+              : 
+              <img src="http://localhost:3000/learnMore.png" alt="Icon Description" className = {styles.toggleOn}></img>
+              }
+        </button>
         {/* Right Section - Detected Symptoms */}
-        <div className={`${styles.detectedSymptomsMainPanel} ${isExpanded ? styles.expandedPanel : ''}`}>
-          <div className={styles.analyticsV2Child4} />
-          <b className={styles.detectedSymptoms}>Detected Symptoms</b>
-          <div className="barz">
-           <div className={styles.analyticsV2Child5} />
-           <div className={styles.analyticsV2Child6} />
-           <div className={styles.analyticsV2Child7} />
-           <div className={styles.analyticsV2Child8} />
-           <div className={styles.analyticsV2Child9} />
-           <div className={styles.analyticsV2Child10} />
-           <img className={styles.ellipseIcon} alt="" src="/ellipse-6.svg" />
-           <div className={styles.analyticsV2Child11} />
-           <img className={styles.analyticsV2Child12} alt="" src="/ellipse-7.svg" />
-           <div className={styles.analyticsV2Child13} />
-           <img className={styles.analyticsV2Child14} alt="" src="/ellipse-9.svg" />
-           <div className={styles.analyticsV2Child15} />
-           <img className={styles.analyticsV2Child16} alt="" src="/ellipse-10.svg" />
-           <div className={styles.analyticsV2Child17} />
-           <img className={styles.analyticsV2Child18} alt="" src="/ellipse-11.svg" />
-           <div className={styles.analyticsV2Child19} />
-           <img className={styles.analyticsV2Child20} alt="" src="/ellipse-12.svg" />
-           <div className={styles.analyticsV2Child21} />
-           <img className={styles.analyticsV2Child22} alt="" src="/ellipse-8.svg" />
-           <div className={styles.analyticsV2Child23} />
-          <div className={styles.analyticsV2Child24} />
-          <img className={styles.analyticsV2Child25} alt="" src="/ellipse-13.svg" />
-          <DetectedSymtomsBars />
+        {!isExpanded && (  
+          <div className={`${styles.detectedSymptomsMainPanel} ${isExpanded ? styles.expandedPanel : ''}`}>
+            <div className={styles.analyticsV2Child4} />
+            <b className={styles.detectedSymptoms}>Detected Symptoms</b>
+            <div className="barz">
+            <div className={styles.analyticsV2Child5} />
+            <div className={styles.analyticsV2Child6} />
+            <div className={styles.analyticsV2Child7} />
+            <div className={styles.analyticsV2Child8} />
+            <div className={styles.analyticsV2Child9} />
+            <div className={styles.analyticsV2Child10} />
+            <img className={styles.ellipseIcon} alt="" src="/ellipse-6.svg" />
+            <div className={styles.analyticsV2Child11} />
+            <img className={styles.analyticsV2Child12} alt="" src="/ellipse-7.svg" />
+            <div className={styles.analyticsV2Child13} />
+            <img className={styles.analyticsV2Child14} alt="" src="/ellipse-9.svg" />
+            <div className={styles.analyticsV2Child15} />
+            <img className={styles.analyticsV2Child16} alt="" src="/ellipse-10.svg" />
+            <div className={styles.analyticsV2Child17} />
+            <img className={styles.analyticsV2Child18} alt="" src="/ellipse-11.svg" />
+            <div className={styles.analyticsV2Child19} />
+            <img className={styles.analyticsV2Child20} alt="" src="/ellipse-12.svg" />
+            <div className={styles.analyticsV2Child21} />
+            <img className={styles.analyticsV2Child22} alt="" src="/ellipse-8.svg" />
+            <div className={styles.analyticsV2Child23} />
+            <div className={styles.analyticsV2Child24} />
+            <img className={styles.analyticsV2Child25} alt="" src="/ellipse-13.svg" />
+            <DetectedSymtomsBars />
+            </div>
           </div>
-          {/* Conditionally rendered Expand/Exit button */}
-          <button onClick={toggleExpand} className={styles.toggleButton}>
-            {isExpanded ? '>' : '<'}
-          </button>
-        </div>
+        )}
       </div>
       
       <img src="http://localhost:3000/Vector.png" alt="Icon Description" className={styles.vectorGraphic}></img>
