3a779a7dfe6d77811c22959c4b2984b1158da033
Jason Chan <jcshun@berkeley.edu>
2023-11-15 23:33:32 -0800

    Base login.js and css

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -19,31 +19,36 @@ function Login() {
   
   return (
     <div className="root">
-      <div className="Navbar" />
       <div className="page-parent">
+        <div className="instructions">
+            <h3>Login</h3>
+            <p>NOTE: TYPE RANDOM STUFF AND CLICK SUBMIT TO GET PAST THIS PAGE</p>
+            <p>NOTE: MAKE SURE TO RUN THE BACKEND LOCALLY OR THIS WILL NOT WORK</p>
+            <p>To Run The Backend: cd into backend, run "npm install", then run "node server.mjs"</p>
+        </div>
         <div className="page-header-container">
           <div className="header-container">
             <div className="logo">
-
+              IMAGE HERE
             </div>
             <div className="BTS-header">
               <div className="BTS-title">
-
+                TITLE HERE
               </div>
               <div className="BTS-description">
-                
+                desc here
               </div>
             </div>
           </div>
         </div>
         <div className="login-form">
           <form onSubmit={handleLogin} >
-            <h3>Login</h3>
-            <p>NOTE: TYPE RANDOM STUFF AND CLICK SUBMIT TO GET PAST THIS PAGE</p>
-            <p>NOTE: MAKE SURE TO RUN THE BACKEND LOCALLY OR THIS WILL NOT WORK</p>
-            <p>To Run The Backend: cd into backend, run "npm install", then run "node server.mjs"</p>
+            <h3>Sign in</h3>
+            <div>Welcome back!</div>
             <div>
+              <label for="username">Login ID</label><br/>
               <input
+                id="username"
                 type="text"
                 placeholder="Username"
                 onChange={(e) =>
@@ -55,7 +60,9 @@ function Login() {
               />
             </div>
             <div>
+              <label for="password">Password</label><br/>
               <input
+                id="password"
                 type="text"
                 placeholder="Password"
                 onChange={(e) =>
@@ -68,7 +75,7 @@ function Login() {
             </div>
             <div>
               <button className="submit-button" type="submit">
-                Submit
+                Sign in
               </button>
             </div>
           </form>
